9b47a11087259a7a78a08f2f81027d0df59ab76b
Andrew Bland <abland4@asu.edu>
2020-05-07 15:47:26 -0700

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,13 @@
+	*************ASSIGNMENT 1 INFO*************** 
+ .NET version: 4.7.2
+.SLN location: tempConvention folder
+	 WFA1: Application to consume TempConvention service
+	 WFA2: Application to consume StringSort service
+       WebApp: Application for Problem 5
+
+	*************ASSIGNMENT 2 INFO*************** 
+ .NET version: 4.7.2
+	Solutions are included in WebApp
+
+
+** All Assignment requirements are implemented in the tempConvention.sln solution file 
---
+++ tempConvention/App_Code/Service.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class Service : IService
+{
+    int IService.F2c(int c)
+    {
+
+        c = (((c - 32) * 5) / 9);
+        return (c);
+    }
+
+     int IService.C2f(int c)
+    {
+        
+        c = (((c * 9) / 5) + 32);
+        return (c);
+    }
+   
+}
